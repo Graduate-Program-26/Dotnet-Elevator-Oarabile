c45c17a7b72cd91064264341e41172f124eaa314
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-06-12 08:35:47 +0200

    feat: created tests and refactor passengerCount

--- src/ElevatorSim.Domain/value-objects/PassengerCount.cs
+++ src/ElevatorSim.Domain/value-objects/PassengerCount.cs
@@ -5,20 +5,11 @@ namespace ElevatorSim.Domain.ValueObjects;
 public readonly record struct PassengerCount
 {
     public int Value { get; }
-    public PassengerCount(int value, int maxCapacity)
+    public PassengerCount(int value)
     {
         if (value < 0)
         {
             throw new ArgumentOutOfRangeException(nameof(value), value, "Passenger count cannot be negative.");
-        }
-
-        if (value > maxCapacity)
-        {
-            throw new CapacityExceededException(
-                elevatorId: "N/A",
-                currentPassengers: 0,
-                maxCapacity: maxCapacity,
-                incomingPassengers: value);
         }
 
         Value = value;
