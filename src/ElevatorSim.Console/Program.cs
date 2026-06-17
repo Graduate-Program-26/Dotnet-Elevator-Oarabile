fe1c317cadbeac78f986cd7278af813fa4ec439d
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-06-17 10:58:15 +0200

    feat: added console startup code

--- src/ElevatorSim.Console/Program.cs
+++ src/ElevatorSim.Console/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ElevatorSim.Application.Extensions;
+using ElevatorSim.Infrastructure.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+ServiceCollection services = new ServiceCollection();
+
+services.AddElevatorApplication();
+services.AddElevatorLogging();
+services.AddElevatorValidation();
+
+await using var serviceProvider = services.BuildServiceProvider();
+
+Console.WriteLine("=== ElevatorSim ===");
+Console.WriteLine("Welcome! Let's configure your building.");
